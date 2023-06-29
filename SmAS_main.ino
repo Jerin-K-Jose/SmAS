08796ffc5b1a153dbf7dbec6cb21dd7dea471596
Jerin-K-Jose <jerinkjos@gmail.com>
2023-06-29 12:47:04 +0530

    Added logic to turn on or off pump

--- SmAS_main.ino
+++ SmAS_main.ino
@@ -7,14 +7,17 @@
 int lcdColumns = 16;
 int lcdRows = 2;
 bool functionExecuted = false;
+
 const int sensor_pin = A0;
 const int pirPin = 12; 
+const int dhtPin = 26;
+const int pumpPin = 13;
 String messageStatic = "SMAS";
 String messageToScroll = "Farming through Technology";
 String pump_status = "";
 
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
-DHT dht(26, DHT11);
+DHT dht(dhtPin, DHT11);
 
 // WiFi credentials
 char auth[] = "UPosKYIbfjiyCjz4peG7tSg4TlTp1D9F";
@@ -40,6 +43,7 @@ void setup()
   lcd.print("Connected!");
   delay(1000);
   lcd.clear();
+  
   // Connect to Blynk server
   Blynk.begin(auth, ssid, password);
   Blynk.syncVirtual(V3);
@@ -48,7 +52,9 @@ void setup()
   lcd.setCursor(0, 0);
   lcd.print("SMAS");
   scrollText(messageToScroll, 300, lcdColumns);
+  
   pinMode(pirPin, INPUT);
+  pinMode(pumpPin, OUTPUT);
   delay(500);
 }
 
@@ -63,22 +69,31 @@ void loop()
   int sensor_analog = analogRead(sensor_pin);
   int moisture = ( 100 - ( (sensor_analog/4095.000) * 100 ) );
   disp(temp, humidity, moisture, pump_status);
+  int pirValue = digitalRead(pirPin); 
   Serial.print("Temp: "); Serial.print(temp); Serial.print(" c"); Serial.print("  Humidity: "); Serial.print(humidity); Serial.print("%"); 
   Serial.print("  Moisture: "); Serial.print(moisture); Serial.print("%"); Serial.println();
   Blynk.virtualWrite(V0, temp);
   Blynk.virtualWrite(V1, humidity);
   Blynk.virtualWrite(V2, moisture);
+  if(moisture < 40 && moisture > 10){
+    digitalWrite(pump, HIGH);
+    pump_status = "ON";
+  }
+  else{
+    digitalWrite(pump, LOW);
+    pump_status = "OFF";
+  }
   if(pump_status == "ON")
     Blynk.virtualWrite(V4, 1);
   else
     Blynk.virtualWrite(V4, 0);
-  int pirValue = digitalRead(pirPin); 
-
   if (pirValue == HIGH) {
     Serial.println("Motion detected!");
+    Blynk.virtualWrite(V5,1);
   }
   else{
     Serial.println("NO Motion");
+    Blynk.virtualWrite(V5, 0);
   }
 }
 
