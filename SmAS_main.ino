d53b482c265d367eaac2d10b0c8abfa0bfd3657a
Jerin-K-Jose <jerinkjos@gmail.com>
2023-07-26 14:03:14 +0530

    Phase 1 Final code

--- SmAS_main.ino
+++ SmAS_main.ino
@@ -9,24 +9,33 @@ int lcdRows = 2;
 bool functionExecuted = false;
 
 const int sensor_pin = A0;
-const int pirPin = 12; 
-const int dhtPin = 26;
-const int pumpPin = 13;
+const int pirPin = 27; 
+const int dhtPin = 14;
+const int pumpPin = 32;
+const int pwm1 = 12;
+const int pwm2 = 13;
 String messageStatic = "SMAS";
 String messageToScroll = "Farming through Technology";
 String pump_status = "";
+int pump_flag = 0;
 
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 DHT dht(dhtPin, DHT11);
 
 // WiFi credentials
 char auth[] = "UPosKYIbfjiyCjz4peG7tSg4TlTp1D9F";
-char ssid[] = "iPhone";
+char ssid[] = "JKJ iPhone";
 char password[] = "9895524301";
 
 void setup()
 {
-  Serial.begin(115200);
+  //Serial.begin(115200);
+  pinMode(pirPin, INPUT);
+  pinMode(pumpPin, OUTPUT);
+  pinMode(pwm1, OUTPUT);
+  pinMode(pwm2, OUTPUT);
+  digitalWrite(pumpPin, LOW);
+  delay(500);
   lcd.init();     
   lcd.backlight();
   
@@ -36,9 +45,9 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.println("Connecting to WiFi...");
+    //Serial.println("Connecting to WiFi...");
   }
-  Serial.println("Connected to WiFi!");
+  //Serial.println("Connected to WiFi!");
   lcd.clear(); lcd.setCursor(0,0);
   lcd.print("Connected!");
   delay(1000);
@@ -53,36 +62,42 @@ void setup()
   lcd.print("SMAS");
   scrollText(messageToScroll, 300, lcdColumns);
   
-  pinMode(pirPin, INPUT);
-  pinMode(pumpPin, OUTPUT);
-  delay(500);
+  ledcAttachPin(pwm1,0); ledcAttachPin(pwm2,1);
+  ledcSetup(0, 50, 8); ledcSetup(1, 50, 8);
+  ledcWrite(0,128); ledcWrite(1, 128);
 }
 
 void loop()
 {
   // Run Blynk background tasks
   Blynk.run();
-  
+  delay(10);
   int temp = dht.readTemperature();
   int humidity = dht.readHumidity();
   delay(1000);
   int sensor_analog = analogRead(sensor_pin);
-  int moisture = ( 100 - ( (sensor_analog/4095.000) * 100 ) );
+  int moisture = map(sensor_analog,4095,850,0,100);
+  delay(100);
   disp(temp, humidity, moisture, pump_status);
   int pirValue = digitalRead(pirPin); 
-  Serial.print("Temp: "); Serial.print(temp); Serial.print(" c"); Serial.print("  Humidity: "); Serial.print(humidity); Serial.print("%"); 
-  Serial.print("  Moisture: "); Serial.print(moisture); Serial.print("%"); Serial.println();
+//  Serial.print("Temp: "); Serial.print(temp); Serial.print(" c"); Serial.print("  Humidity: "); Serial.print(humidity); Serial.print("%"); 
+//  Serial.print("  Moisture: "); Serial.print(moisture); Serial.print("%"); Serial.println();
   Blynk.virtualWrite(V0, temp);
   Blynk.virtualWrite(V1, humidity);
   Blynk.virtualWrite(V2, moisture);
-  if(moisture < 40 && moisture > 10){
-    digitalWrite(pump, HIGH);
+  if(pump_flag == 0){
+    if(moisture < 50 && moisture > 10){
+    digitalWrite(pumpPin, HIGH);
     pump_status = "ON";
+    delay(10);
   }
   else{
-    digitalWrite(pump, LOW);
+    digitalWrite(pumpPin, LOW);
     pump_status = "OFF";
+    delay(10);
   }
+  }
+  delay(10);
   if(pump_status == "ON")
     Blynk.virtualWrite(V4, 1);
   else
@@ -95,6 +110,7 @@ void loop()
     Serial.println("NO Motion");
     Blynk.virtualWrite(V5, 0);
   }
+  delay(100);
 }
 
 // Function to scroll text
@@ -112,19 +128,33 @@ void scrollText(String message, int delayTime, int lcdColumns) {
 }
 
 void disp(int temp, int humidity, int moisture, String pump_status){
+  delay(100);
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("T:"); lcd.print(temp); lcd.print("C");
-  lcd.print(" H:"); lcd.print(humidity); lcd.print("%");
+  lcd.print("T:"); delay(10); lcd.print(temp); delay(10); lcd.print("C");
+  delay(10);
+  lcd.print(" H:"); delay(10); lcd.print(humidity); delay(10); lcd.print("%");
+  delay(10);
   lcd.setCursor(0,1);
-  lcd.print("M:"); lcd.print(moisture); lcd.print("%");
-  lcd.print(" P:"); lcd.print(pump_status);
+  lcd.print("M:"); delay(10); lcd.print(moisture); delay(10); lcd.print("%");
+  delay(10);
+  lcd.print(" P:"); delay(10); lcd.print(pump_status);
 }
 
 BLYNK_WRITE(V3){
   int value = param.asInt();
-  if(value)
+  if(value){
+    delay(10);
     pump_status = "ON";
-  else
+    digitalWrite(pumpPin, HIGH);
+    pump_flag = 1;
+    delay(10);
+  }
+  else{
+    delay(10);
     pump_status = "OFF";
+    digitalWrite(pumpPin, LOW);
+    pump_flag = 0;
+    delay(10);
+  }
 }
